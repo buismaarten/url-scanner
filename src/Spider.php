2e0aea5e1df296e4650c9752b45d5bbfc152924c
Maarten Buis <buismaarten@outlook.com>
2023-11-03 19:41:44 +0100

    wip

---
+++ src/Spider.php
@@ -0,0 +1,31 @@
+<?php
+
+class Spider
+{
+    private AbstractDownloader $downloader;
+    private DiscovererCollection $discovererCollection;
+
+    public function __construct(AbstractDownloader $downloader = null, DiscovererCollection $discovererCollection = null)
+    {
+        $this->downloader = ($downloader ?? new NativeDownloader);
+        $this->discovererCollection = ($discovererCollection ?? new DiscovererCollection);
+    }
+
+    public function getDownloader(): AbstractDownloader
+    {
+        return $this->downloader;
+    }
+
+    public function getDiscovererCollection(): DiscovererCollection
+    {
+        return $this->discovererCollection;
+    }
+
+    public function crawl(string $url): array
+    {
+        $resource = $this->getDownloader()->download($url);
+        $results = $this->getDiscovererCollection()->discover($resource);
+
+        return $results;
+    }
+}
--- test.php
+++ test.php
@@ -15,38 +15,7 @@ require_once __DIR__.'/src/Filters/AllowedSchemeFilter.php';
 
 require_once __DIR__.'/src/DiscovererCollection.php';
 require_once __DIR__.'/src/Resource.php';
-
-
-
-class Spider
-{
-    private AbstractDownloader $downloader;
-    private DiscovererCollection $discovererCollection;
-
-    public function __construct(AbstractDownloader $downloader = null, DiscovererCollection $discovererCollection = null)
-    {
-        $this->downloader = ($downloader ?? new NativeDownloader);
-        $this->discovererCollection = ($discovererCollection ?? new DiscovererCollection);
-    }
-
-    public function getDownloader(): AbstractDownloader
-    {
-        return $this->downloader;
-    }
-
-    public function getDiscovererCollection(): DiscovererCollection
-    {
-        return $this->discovererCollection;
-    }
-
-    public function crawl(string $url): array
-    {
-        $resource = $this->getDownloader()->download($url);
-        $results = $this->getDiscovererCollection()->discover($resource);
-
-        return $results;
-    }
-}
+require_once __DIR__.'/src/Spider.php';
 
 
 $spider = new Spider;
