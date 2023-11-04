f48e403ceed00bd0a1be4cb21f239abe711ca8d0
Maarten Buis <buismaarten@outlook.com>
2023-11-04 08:48:32 +0100

    wip

--- src/Filters/AllowedHostsFilter.php
+++ src/Filters/AllowedHostsFilter.php
@@ -5,7 +5,7 @@ class AllowedHostsFilter extends AbstractFilter
     /** @var string[] */
     private array $allowedHosts = [];
 
-    public function __construct(array $allowedHosts = [])
+    public function __construct(array $allowedHosts)
     {
         $this->allowedHosts = $allowedHosts;
     }
