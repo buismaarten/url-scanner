ead2030d0840d6353aad05b05161300e1e15a411
Maarten Buis <buismaarten@outlook.com>
2023-11-13 22:59:53 +0100

    wip

--- test.php
+++ test.php
@@ -9,7 +9,7 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $crawler = new Crawler;
 $crawler->addDiscoverer(new Discoverer\CssSelectorDiscoverer('a[href]', 'href'));
-$crawler->addFilter(new Filter\AllowedSchemesFilter(['http', 'https']));
+$crawler->addFilter(new Filter\AllowedSchemesFilter(['https']));
 
 foreach ($crawler->crawl('https://laravel.com/docs') as $url) {
     echo $url."\n";
