aff3f22ad84cdde0da465fd4dbec60b8342792f0
Maarten Buis <buismaarten@outlook.com>
2023-11-21 18:25:06 +0100

    wip

---
+++ src/Crawler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Buismaarten\Crawler;
+
+use Buismaarten\Crawler\Discoverers\AbstractDiscoverer;
+use League\Uri\Contracts\UriInterface;
+use League\Uri\Uri;
+use Symfony\Component\DomCrawler\Crawler as DomCrawler;
+
+class Crawler
+{
+    private DomCrawler $domCrawler;
+    private ?string $baseUrl;
+
+    public function __construct(string $html, ?string $baseUrl = null)
+    {
+        $this->domCrawler = new DomCrawler($html);
+        $this->baseUrl = $baseUrl;
+    }
+
+    public function crawl(AbstractDiscoverer $discoverer): array
+    {
+        $discoveredUrls = $discoverer->discover($this->domCrawler);
+        $urls = [];
+
+        foreach ($discoveredUrls as $discoveredUrl) {
+            $url = Uri::fromBaseUri($discoveredUrl, $this->baseUrl);
+
+            // @todo
+            if (str_starts_with($url, 'http')) {
+                $urls[] = $url;
+            }
+        }
+
+        $urls = array_map(fn (UriInterface $url) => $url->toString(), $urls);
+        $urls = array_unique($urls);
+        $urls = array_values($urls);
+
+        return $urls;
+    }
+}
--- test.php
+++ test.php
@@ -1,51 +1,14 @@
 <?php
 
-use Buismaarten\Crawler\Discoverers\AbstractDiscoverer;
+use Buismaarten\Crawler\Crawler;
 use Buismaarten\Crawler\Discoverers\CssSelectorDiscoverer;
-use League\Uri\Contracts\UriInterface;
-use League\Uri\Uri;
-use Symfony\Component\DomCrawler\Crawler;
 
 require_once __DIR__.'/vendor/autoload.php';
 
 
-class TestCrawler
-{
-    private Crawler $crawler;
-    private ?string $baseUrl;
-
-    public function __construct(string $html, ?string $baseUrl = null)
-    {
-        $this->crawler = new Crawler($html);
-        $this->baseUrl = $baseUrl;
-    }
-
-    public function crawl(AbstractDiscoverer $discoverer): array
-    {
-        $discoveredUrls = $discoverer->discover($this->crawler);
-        $urls = [];
-
-        foreach ($discoveredUrls as $discoveredUrl) {
-            $url = Uri::fromBaseUri($discoveredUrl, $this->baseUrl);
-
-            // @todo
-            if (str_starts_with($url, 'http')) {
-                $urls[] = $url;
-            }
-        }
-
-        $urls = array_map(fn (UriInterface $url) => $url->toString(), $urls);
-        $urls = array_unique($urls);
-        $urls = array_values($urls);
-
-        return $urls;
-    }
-}
-
-
 $baseUrl = 'https://letsbuildit.nl';
 $html = file_get_contents($baseUrl);
-$crawler = new TestCrawler($html, $baseUrl);
+$crawler = new Crawler($html, $baseUrl);
 
 print_r($crawler->crawl(new CssSelectorDiscoverer('a[href]', 'href')));
 exit;
