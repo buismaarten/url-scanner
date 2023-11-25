61b6ec9f82bd37ed397b7ce1785bc3974a1109f1
Maarten Buis <buismaarten@outlook.com>
2023-11-25 08:30:47 +0100

    wip

--- test.php
+++ test.php
@@ -10,4 +10,4 @@ $baseUrl = 'https://transip.nl';
 $crawler = new Crawler(file_get_contents($baseUrl), $baseUrl);
 
 print_r($crawler->getUrls(new CssSelectorDiscoverer('a[href]', 'href')));
-print_r($crawler->getHosts(new CssSelectorDiscoverer('[href]', 'href')));
+print_r($crawler->getHosts(new CssSelectorDiscoverer('*[href]', 'href')));
