9136a20b666c836aa6a6552be1eb9e1e2fc4078d
Maarten Buis <buismaarten@outlook.com>
2023-11-27 18:59:05 +0100

    wip

--- src/Detectors/SymfonyDetector.php
+++ src/Detectors/SymfonyDetector.php
@@ -17,10 +17,22 @@ final class SymfonyDetector extends AbstractDetector
     {
         return [
             '//a[@href]' => 'href',
+            '//audio[@src]' => 'src',
+            '//iframe[@src]' => 'src',
             '//img[@src]' => 'src',
             '//link[@href]' => 'href',
+            '//meta[@property="og:audio:secure_url"][@content]' => 'content',
+            '//meta[@property="og:audio"][@content]' => 'content',
+            '//meta[@property="og:image:secure_url"][@content]' => 'content',
+            '//meta[@property="og:image"][@content]' => 'content',
+            '//meta[@property="og:url"][@content]' => 'content',
+            '//meta[@property="og:video:secure_url"][@content]' => 'content',
+            '//meta[@property="og:video"][@content]' => 'content',
             '//script[@src]' => 'src',
-            '//iframe[@src]' => 'src',
+            '//source[@src]' => 'src',
+            '//track[@src]' => 'src',
+            '//video[@poster]' => 'poster',
+            '//video[@src]' => 'src',
         ];
     }
 }
