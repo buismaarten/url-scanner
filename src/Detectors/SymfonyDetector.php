88730e7c5272d9decc220c879d2daf3269eef189
Maarten Buis <buismaarten@outlook.com>
2023-12-04 21:33:26 +0100

    wip

--- src/Detectors/SymfonyDetector.php
+++ src/Detectors/SymfonyDetector.php
@@ -9,7 +9,34 @@ use Symfony\Component\DomCrawler\Crawler;
 
 final class SymfonyDetector extends AbstractDetector
 {
-    // @todo
+    /** @var array<string, string> */
+    private static $filters = [
+        '//a[@href]' => 'href',
+        '//audio[@src]' => 'src',
+        '//base[@href]' => 'href',
+        '//button[@formaction]' => 'formaction',
+        '//form[@action]' => 'action',
+        '//iframe[@src]' => 'src',
+        '//img[@src]' => 'src',
+        '//input[@formaction]' => 'formaction',
+        '//input[@src]' => 'src',
+        '//link[@href]' => 'href',
+        '//meta[@name="msapplication-TileImage"][@content]' => 'content',
+        '//meta[@name="twitter:image"][@content]' => 'content',
+        '//meta[@property="og:audio:secure_url"][@content]' => 'content',
+        '//meta[@property="og:audio"][@content]' => 'content',
+        '//meta[@property="og:image:secure_url"][@content]' => 'content',
+        '//meta[@property="og:image"][@content]' => 'content',
+        '//meta[@property="og:url"][@content]' => 'content',
+        '//meta[@property="og:video:secure_url"][@content]' => 'content',
+        '//meta[@property="og:video"][@content]' => 'content',
+        '//script[@src]' => 'src',
+        '//source[@src]' => 'src',
+        '//track[@src]' => 'src',
+        '//video[@poster]' => 'poster',
+        '//video[@src]' => 'src',
+    ];
+
     /** @return iterable<string> */
     public function detect(UriInterface $url): iterable
     {
@@ -22,39 +49,8 @@ final class SymfonyDetector extends AbstractDetector
         $content = $response->getBody()->getContents();
         $crawler = new Crawler($content, $url->toString());
 
-        foreach (self::getFilters() as $query => $attribute) {
+        foreach (self::$filters as $query => $attribute) {
             yield from $crawler->filterXPath($query)->each(fn (Crawler $node) => ($node->attr($attribute) ?? ''));
         }
-    }
-
-    /** @return array<string, string> */
-    private static function getFilters(): array
-    {
-        return [
-            '//a[@href]' => 'href',
-            '//audio[@src]' => 'src',
-            '//base[@href]' => 'href',
-            '//button[@formaction]' => 'formaction',
-            '//form[@action]' => 'action',
-            '//iframe[@src]' => 'src',
-            '//img[@src]' => 'src',
-            '//input[@formaction]' => 'formaction',
-            '//input[@src]' => 'src',
-            '//link[@href]' => 'href',
-            '//meta[@name="msapplication-TileImage"][@content]' => 'content',
-            '//meta[@name="twitter:image"][@content]' => 'content',
-            '//meta[@property="og:audio:secure_url"][@content]' => 'content',
-            '//meta[@property="og:audio"][@content]' => 'content',
-            '//meta[@property="og:image:secure_url"][@content]' => 'content',
-            '//meta[@property="og:image"][@content]' => 'content',
-            '//meta[@property="og:url"][@content]' => 'content',
-            '//meta[@property="og:video:secure_url"][@content]' => 'content',
-            '//meta[@property="og:video"][@content]' => 'content',
-            '//script[@src]' => 'src',
-            '//source[@src]' => 'src',
-            '//track[@src]' => 'src',
-            '//video[@poster]' => 'poster',
-            '//video[@src]' => 'src',
-        ];
     }
 }
