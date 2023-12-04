8b1035ae152ae008fd1e275419092f29ea6ec498
Maarten Buis <buismaarten@outlook.com>
2023-12-04 21:44:31 +0100

    wip

--- src/UrlScanner.php
+++ src/UrlScanner.php
@@ -6,6 +6,8 @@ namespace Buismaarten\UrlScanner;
 
 use Buismaarten\UrlScanner\Detectors\AbstractDetector;
 use Buismaarten\UrlScanner\Detectors\SymfonyDetector;
+use League\Uri\Contracts\UriInterface;
+use League\Uri\Exceptions\SyntaxError;
 use League\Uri\Uri;
 
 final class UrlScanner
@@ -31,6 +33,7 @@ final class UrlScanner
         return new SymfonyDetector;
     }
 
+    // @todo
     /** @return string[] */
     public function scan(string $url): array
     {
@@ -38,9 +41,31 @@ final class UrlScanner
         $normalizedUrls = [];
 
         foreach ($detectedUrls as $detectedUrl) {
-            $normalizedUrls[] = Uri::fromBaseUri($detectedUrl, $url)->toString();
+            $normalizedUrl = self::normalizeUrl($detectedUrl, $url);
+
+            if ($normalizedUrl !== null) {
+                $normalizedUrls[] = $normalizedUrl->toString();
+            }
         }
 
         return $normalizedUrls;
+    }
+
+    private static function normalizeUrl(string $url, ?string $baseUrl): ?UriInterface
+    {
+        try {
+            $components = Uri::fromBaseUri($url, $baseUrl)->getComponents();
+            $components['path'] = rtrim($components['path'], '/');
+        } catch (SyntaxError) {
+            return null;
+        }
+
+        return Uri::fromComponents([
+            'scheme' => $components['scheme'],
+            'host'   => $components['host'],
+            'port'   => $components['port'],
+            'path'   => $components['path'],
+            'query'  => $components['query'],
+        ]);
     }
 }
