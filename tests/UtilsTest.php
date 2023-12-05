d32e56a27a41920873f29f950460ebebef85ccab
Maarten Buis <buismaarten@outlook.com>
2023-12-05 17:19:47 +0100

    wip

---
+++ tests/UtilsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+// @todo
+final class UtilsTest extends TestCase
+{
+    public function test_1(): void
+    {
+        $this->assertTrue(true);
+    }
+}
