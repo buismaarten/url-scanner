4fb0cf86f130f00bdf0572430d24d59db5466785
Maarten Buis <buismaarten@outlook.com>
2023-12-05 20:55:46 +0100

    wip

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -2,13 +2,31 @@
 
 declare(strict_types=1);
 
+use Buismaarten\UrlScanner\Utils;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
-// @todo
 final class UtilsTest extends TestCase
 {
-    public function test_1(): void
+    #[DataProvider('providerNormalizeUrl')]
+    public function testNormalizeUrl(?string $expected, array $parameters): void
     {
-        $this->assertTrue(true);
+        $this->assertSame($expected, Utils::normalizeUrl($parameters[0], $parameters[1])?->toString());
+    }
+
+    public static function providerNormalizeUrl(): array
+    {
+        return [
+            // Relative
+            ['https://localhost', ['/', 'https://localhost']],
+            ['https://localhost/test', ['/test', 'https://localhost']],
+
+            // Absolute
+            ['https://localhost', ['https://localhost', null]],
+            ['https://localhost/test', ['https://localhost/test', null]],
+
+            // Invalid
+            [null, ['://localhost', null]],
+        ];
     }
 }
