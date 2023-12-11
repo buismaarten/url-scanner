cd1f8ba0af941785cd6fc0f75d796975ab38a1e7
Maarten Buis <buismaarten@outlook.com>
2023-12-11 22:00:17 +0100

    wip

--- tests/Detectors/SymfonyDetectorTest.php
+++ tests/Detectors/SymfonyDetectorTest.php
@@ -6,7 +6,6 @@ use Buismaarten\UrlScanner\Detectors\SymfonyDetector;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
-// @todo
 final class SymfonyDetectorTest extends TestCase
 {
     #[DataProvider('providerDetect')]
@@ -27,6 +26,78 @@ final class SymfonyDetectorTest extends TestCase
                 'content' => '<a href="/path">Link</a>',
                 'expected' => [
                     '/path',
+                ],
+            ],
+            [
+                'content' => '<audio src="/path/to/audio.mp3"></audio>',
+                'expected' => [
+                    '/path/to/audio.mp3',
+                ],
+            ],
+            [
+                'content' => '<base href="/path"></base>',
+                'expected' => [
+                    '/path',
+                ],
+            ],
+
+            // @todo: add tests for forms
+
+            [
+                'content' => '<iframe src="/path"></iframe>',
+                'expected' => [
+                    '/path',
+                ],
+            ],
+            [
+                'content' => '<img src="/path/to/image.jpg">',
+                'expected' => [
+                    '/path/to/image.jpg',
+                ],
+            ],
+            [
+                'content' => '<input src="/path/to/image.jpg">',
+                'expected' => [
+                    '/path/to/image.jpg',
+                ],
+            ],
+            [
+                'content' => '<link rel="stylesheet" href="/path/to/stylesheet.css">',
+                'expected' => [
+                    '/path/to/stylesheet.css',
+                ],
+            ],
+
+            // @todo: add tests for meta tags
+
+            [
+                'content' => '<script src="/path/to/script.js"></script>',
+                'expected' => [
+                    '/path/to/script.js',
+                ],
+            ],
+            [
+                'content' => '<source src="/path/to/video.mp4">',
+                'expected' => [
+                    '/path/to/video.mp4',
+                ],
+            ],
+            [
+                'content' => '<track src="/path/to/track.vtt">',
+                'expected' => [
+                    '/path/to/track.vtt',
+                ],
+            ],
+            [
+                'content' => '<video poster="/path/to/poster.jpg"></video>',
+                'expected' => [
+                    '/path/to/poster.jpg',
+                ],
+            ],
+            [
+                'content' => '<video src="/path/to/video.mp4"></video>',
+                'expected' => [
+                    '/path/to/video.mp4',
                 ],
             ],
         ];
