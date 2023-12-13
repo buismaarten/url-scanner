f4ceef302fbad85a55dac53858998814f9d5a3ad
Maarten Buis <buismaarten@outlook.com>
2023-12-13 23:20:26 +0100

    wip

--- src/Detectors/RegexDetector.php
+++ src/Detectors/RegexDetector.php
@@ -38,10 +38,10 @@ final class RegexDetector extends AbstractDetector
             }
         }
 
-        if (preg_match_all('/<!--.*?(https?:\/\/\S+).*?-->/', $this->content, $matches, PREG_SET_ORDER)) {
-            foreach ($matches as $match) {
-                yield $match[1];
-            }
-        }
+        // if (preg_match_all('/<!--.*?(https?:\/\/\S+).*?-->/', $this->content, $matches, PREG_SET_ORDER)) {
+        //     foreach ($matches as $match) {
+        //         yield $match[1];
+        //     }
+        // }
     }
 }
