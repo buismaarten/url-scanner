75ede7563f540f961ef84a92c710fc0f14ecb61b
Maarten Buis <buismaarten@outlook.com>
2023-12-15 20:12:53 +0100

    wip

--- src/Detectors/RegexDetector.php
+++ src/Detectors/RegexDetector.php
@@ -40,7 +40,7 @@ final class RegexDetector extends AbstractDetector
 
         if (preg_match_all('/@import\s+(?:\'|")(https?:\/\/[^\'"]+)(?:\'|")/', $this->content, $matches, PREG_SET_ORDER)) {
             foreach ($matches as $match) {
-                $value = $match[1];
+                $value = preg_replace('/^[\'"]+|[\'"]+$/', '', $match[1]);
 
                 if (is_string($value) && ! empty($value)) {
                     yield trim($value);
