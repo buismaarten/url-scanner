ccfa77ec95d7daf1ed1e178d08165c91260a4965
Maarten Buis <buismaarten@outlook.com>
2023-12-18 18:50:04 +0100

    wip

--- demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$json_string = file_get_contents('https://letsbuildit.nl');
-
-$pattern = '/":\s*"((https?:\/\/[^"]+))"/';
-preg_match_all($pattern, $json_string, $matches);
-
-$urls = $matches[1];
-print_r($urls);
