a8e5c7921fe9d01606833dcb5469a9a7eb4098af
Maarten Buis <buismaarten@outlook.com>
2023-12-25 17:26:22 +0100

    wip

--- src/Downloaders/NativeDownloader.php
+++ src/Downloaders/NativeDownloader.php
@@ -10,10 +10,13 @@ class NativeDownloader implements DownloaderInterface
 {
     public function download(string $url): string
     {
-        if ($body = file_get_contents($url)) {
-            return $body;
+        // @todo
+        $body = file_get_contents($url, length: (5 * 1024 * 1024));
+
+        if ($body === false) {
+            return '';
         }
 
-        return '';
+        return $body;
     }
 }
