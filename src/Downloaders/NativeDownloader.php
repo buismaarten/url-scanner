6f000766840c07b5f352bb9ea2b64979a7054204
Maarten Buis <buismaarten@outlook.com>
2023-12-26 11:38:26 +0100

    wip

--- src/Downloaders/NativeDownloader.php
+++ src/Downloaders/NativeDownloader.php
@@ -12,8 +12,22 @@ final class NativeDownloader extends AbstractDownloader
     {
         $body = false;
 
+        // @todo
         if (Utils::validateUrl($url)) {
-            $body = file_get_contents($url, length: $this->getLength());
+            $context = stream_context_create([
+                'http' => [
+                    'method' => 'GET',
+                    'header' => [
+                        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3',
+                    ],
+                ],
+            ]);
+
+            $body = file_get_contents(
+                filename: $url,
+                context:  $context,
+                length:   $this->getLength(),
+            );
         }
 
         if ($body === false) {
