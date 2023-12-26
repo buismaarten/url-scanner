6f7e0420c06b8717c20a25ee98cd14c8ed4a66a7
Maarten Buis <buismaarten@outlook.com>
2023-12-26 12:20:19 +0100

    wip

--- src/Downloaders/NativeDownloader.php
+++ src/Downloaders/NativeDownloader.php
@@ -18,7 +18,7 @@ final class NativeDownloader extends AbstractDownloader
                 'http' => [
                     'method' => 'GET',
                     'header' => [
-                        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3',
+                        "User-Agent: {$this->getUserAgent()}",
                     ],
                 ],
             ]);
