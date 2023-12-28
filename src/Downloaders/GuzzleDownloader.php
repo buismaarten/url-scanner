242767ebbc887894e186a73d0a13723caa63e474
Maarten Buis <buismaarten@outlook.com>
2023-12-28 14:54:18 +0100

    wip

--- src/Downloaders/GuzzleDownloader.php
+++ src/Downloaders/GuzzleDownloader.php
@@ -8,18 +8,39 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\TransferException;
 use GuzzleHttp\Psr7\Request;
 
-// @todo
 class GuzzleDownloader extends AbstractDownloader
 {
+    /** @phpstan-ignore-next-line */
+    private array $config;
+
     public function download(string $url): string
     {
         try {
-            $client = new Client;
+            $client = new Client($this->getConfig());
             $response = $client->send(new Request('GET', $url));
         } catch (TransferException) {
             return '';
         }
 
         return $response->getBody()->read($this->getLength());
+    }
+
+    /** @phpstan-ignore-next-line */
+    public function setConfig(array $config): void
+    {
+        $this->config = $config;
+    }
+
+    /** @phpstan-ignore-next-line */
+    private function getConfig(): array
+    {
+        return ($this->config ??= $this->getDefaultConfig());
+    }
+
+    /** @phpstan-ignore-next-line */
+    private function getDefaultConfig(): array
+    {
+        // @todo
+        return [];
     }
 }
