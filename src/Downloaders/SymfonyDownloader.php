00d4bfe6c5148e549edf1f8f56961c5124ceea96
Maarten Buis <buismaarten@outlook.com>
2023-12-30 11:39:47 +0100

    wip

--- src/Downloaders/SymfonyDownloader.php
+++ src/Downloaders/SymfonyDownloader.php
@@ -10,30 +10,29 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class SymfonyDownloader extends AbstractDownloader
 {
-    // @todo
     public function download(string $url): string
     {
         try {
             $client = HttpClient::create();
             $response = $client->request('GET', $url);
-
-            $stream = StreamWrapper::createResource($response);
-            if (! is_resource($stream)) {
-                return '';
-            }
-
-            $content = stream_get_contents($stream, $this->getLength());
-            if ($content === false) {
-                $content = '';
-            }
-
-            if (is_resource($stream)) {
-                fclose($stream);
-            }
-
-            return $content;
         } catch (TransportExceptionInterface) {
             return '';
         }
+
+        $stream = StreamWrapper::createResource($response);
+        if (! is_resource($stream)) {
+            return '';
+        }
+
+        $content = stream_get_contents($stream, $this->getLength());
+        if ($content === false) {
+            $content = '';
+        }
+
+        if (is_resource($stream)) {
+            fclose($stream);
+        }
+
+        return $content;
     }
 }
