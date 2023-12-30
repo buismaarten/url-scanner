fb905c606436fd8d5a255c6d3f4696e1f3272556
Maarten Buis <buismaarten@outlook.com>
2023-12-30 11:18:15 +0100

    wip

--- src/Downloaders/SymfonyDownloader.php
+++ src/Downloaders/SymfonyDownloader.php
@@ -5,18 +5,33 @@ declare(strict_types=1);
 namespace Buismaarten\UrlScanner\Downloaders;
 
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpClient\Response\StreamWrapper;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class SymfonyDownloader extends AbstractDownloader
 {
+    // @todo
     public function download(string $url): string
     {
-        // @todo
         try {
             $client = HttpClient::create();
             $response = $client->request('GET', $url);
 
-            return substr($response->getContent(), 0, $this->getLength());
+            $stream = StreamWrapper::createResource($response);
+            if (! is_resource($stream)) {
+                return '';
+            }
+
+            $content = stream_get_contents($stream, $this->getLength());
+            if ($content === false) {
+                $content = '';
+            }
+
+            if (is_resource($stream)) {
+                fclose($stream);
+            }
+
+            return $content;
         } catch (TransportExceptionInterface) {
             return '';
         }
