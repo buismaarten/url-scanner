218c7a5bb3c26acb389fd316755095a99776b405
Maarten Buis <buismaarten@outlook.com>
2024-01-03 16:05:15 +0100

    wip

--- tests/Downloaders/SymfonyDownloaderTest.php
+++ tests/Downloaders/SymfonyDownloaderTest.php
@@ -3,14 +3,15 @@
 declare(strict_types=1);
 
 use Buismaarten\UrlScanner\Downloaders\SymfonyDownloader;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
 
-// @todo
 class SymfonyDownloaderTest extends TestCase
 {
-    public function test(): void
+    // @todo
+    public function testResponse(): void
     {
         $expected = 'Hello World!';
 
@@ -24,5 +25,37 @@ class SymfonyDownloaderTest extends TestCase
             expected: $expected,
             actual: $downloader->download('https://localhost'),
         );
+    }
+
+    #[DataProvider('responseLengthProvider')]
+    public function testResponseLength(string $expected, string $input, int $length): void
+    {
+        $client = new MockHttpClient;
+        $client->setResponseFactory(new MockResponse($input));
+
+        $downloader = new SymfonyDownloader;
+        $downloader->setClient($client);
+        $downloader->setLength($length);
+
+        $this->assertSame(
+            expected: $expected,
+            actual: $downloader->download('https://localhost'),
+        );
+    }
+
+    public static function responseLengthProvider(): array
+    {
+        return [
+            [
+                'expected' => 'Hello World!',
+                'input' => 'Hello World!',
+                'length' => 12,
+            ],
+            [
+                'expected' => 'Hello',
+                'input' => 'Hello World!',
+                'length' => 5,
+            ],
+        ];
     }
 }
